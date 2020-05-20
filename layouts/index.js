f6e8f828eee0364393548ec481276736394dfafa
徐远翔 <xuyuanxiang@shouqianba.com>
2020-05-20 19:21:29 +0800

    feat: add @ant-design/react-native

---
+++ layouts/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {BackButton} from 'umi';
+
+export default function Layout({children}) {
+  return (
+    <BackButton>
+      {children}
+    </BackButton>
+  )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,24 @@
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
-import {connect} from 'umi';
+import {View, Text} from 'react-native';
+import {List, WhiteSpace} from '@ant-design/react-native';
+import {connect, Link} from 'umi';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const Item = List.Item;
 
 const ConnectedIndexPage = connect(({foo, loading: {effects}}) => ({
   text: foo.text,
   loading: effects['foo/fetch'],
 }))(({text, loading}) => (
-  <View style={styles.container}>
-    <Text>{loading ? 'Loading...' : text}</Text>
+  <View style={{paddingTop: 88}}>
+    <WhiteSpace size="xl" />
+    <List renderHeader={() => <Text>{loading ? 'Loading...' : text}</Text>}>
+      <Link to="/home" component={Item} arrow="horizontal">
+        主页
+      </Link>
+      <Link to="/login" component={Item} arrow="horizontal">
+        登录页
+      </Link>
+    </List>
   </View>
 ));
 
